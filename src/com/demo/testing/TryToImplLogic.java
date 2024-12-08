e46cf02f3380dde36009dc405ca6c34abc3af9d9
vijay <iamvijaygirange@gmail.com>
2024-12-08 19:24:58 +0530

    addtryimpl class

---
+++ src/com/demo/testing/TryToImplLogic.java
@@ -0,0 +1,17 @@
+package com.demo.testing;
+
+import java.util.Scanner;
+
+public class TryToImplLogic {
+	public static void main(String[] args) {
+
+		// get input from user
+		Scanner sc = new Scanner(System.in);
+
+		// try to implement the main logic is here ]
+		// step 1 get input from user
+		System.out.println("Enter name : ");
+		String name = sc.toString();
+
+	}
+}
